d77edb0a3ebeafdf74b82d7df0cc88e96a6261b8
MNOWAK1234 <tarask2002@interia.pl>
2024-01-14 13:12:24 +0100

    Updated IP adresses

--- Frontend/Admin Panel/src/ApiService.js
+++ Frontend/Admin Panel/src/ApiService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class ApiService {
   static async getAllData() {
     try {
-      const response = await axios.get('http://127.0.0.1:5000/get_all_data');
+      const response = await axios.get('http://192.168.1.36:5000/get_all_data');
       return response.data;
     } catch (error) {
       throw new Error('Failed to get all data');
@@ -12,7 +12,7 @@ class ApiService {
 
   static async getWorkerData(username) {
     try {
-      const response = await axios.get(`http://127.0.0.1:5000/get_worker_data/${username}`);
+      const response = await axios.get(`http://192.168.1.36:5000/get_worker_data/${username}`);
       return response.data;
     } catch (error) {
       throw new Error('Failed to get worker data');
@@ -21,7 +21,7 @@ class ApiService {
 
   static async getDataByTimestamp(start_time, end_time) {
     try {
-      const response = await axios.get(`http://127.0.0.1:5000/get_data_by_timestamp/${start_time}/${end_time}`);
+      const response = await axios.get(`http://192.168.1.36:5000/get_data_by_timestamp/${start_time}/${end_time}`);
       return response.data;
     } catch (error) {
       throw new Error('Failed to get data by timestamp');
@@ -30,7 +30,7 @@ class ApiService {
 
   static async getDataByWorkerAndTimestamps(worker_name, start_time, end_time) {
     try {
-      const response = await axios.get(`http://127.0.0.1:5000/get_data_by_worker_and_timestamps/${worker_name}/${start_time}/${end_time}`);
+      const response = await axios.get(`http://192.168.1.36:5000/get_data_by_worker_and_timestamps/${worker_name}/${start_time}/${end_time}`);
       return response.data;
     } catch (error) {
       throw new Error('Failed to get data by worker and timestamps');
@@ -39,7 +39,7 @@ class ApiService {
 
   static async getWorkersAtWork(datestamp) {
     try {
-      const response = await axios.get(`http://127.0.0.1:5000/get_workers_at_work/${datestamp}`);
+      const response = await axios.get(`http://192.168.1.36:5000/get_workers_at_work/${datestamp}`);
       return response.data;
     } catch (error) {
       throw new Error('Failed to get workers at work');
@@ -48,7 +48,7 @@ class ApiService {
 
   static async countAppearancesByTimestamp(start_time, end_time) {
     try {
-      const response = await axios.get(`http://127.0.0.1:5000/count_appearances_by_timestamp/${start_time}/${end_time}`);
+      const response = await axios.get(`http://192.168.1.36:5000/count_appearances_by_timestamp/${start_time}/${end_time}`);
       return response.data;
     } catch (error) {
       throw new Error('Failed to count appearances by timestamp');
